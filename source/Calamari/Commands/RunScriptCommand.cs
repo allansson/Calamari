44870e62d84f9ce48dab714402f83cd486c66de6
Johan Suleiko Allansson <allansson@gmail.com>
2015-09-23 23:39:51 +0200

    Load and save variables when executing run-script command

--- source/Calamari/Commands/RunScriptCommand.cs
+++ source/Calamari/Commands/RunScriptCommand.cs
@@ -13,6 +13,7 @@ namespace Calamari.Commands
     public class RunScriptCommand : Command
     {
         private string variablesFile;
+        private string outputVariablesFile;
         private string scriptFile;
         private string sensitiveVariablesFile;
         private string sensitiveVariablesPassword;
@@ -20,6 +21,7 @@ namespace Calamari.Commands
         public RunScriptCommand()
         {
             Options.Add("variables=", "Path to a JSON file containing variables.", v => variablesFile = Path.GetFullPath(v));
+            Options.Add("outputVariables=", "Path to JSON file where output variables are stored.", v => outputVariablesFile = Path.GetFullPath(v));
             Options.Add("script=", "Path to the script (PowerShell or ScriptCS) script to execute.", v => scriptFile = Path.GetFullPath(v));
             Options.Add("sensitiveVariables=", "Password protected JSON file containing sensitive-variables.", v => sensitiveVariablesFile = v);
             Options.Add("sensitiveVariablesPassword=", "Password used to decrypt sensitive-variables.", v => sensitiveVariablesPassword = v);
@@ -30,21 +32,27 @@ namespace Calamari.Commands
             Options.Parse(commandLineArguments);
 
             var variables = new CalamariVariableDictionary(variablesFile, sensitiveVariablesFile, sensitiveVariablesPassword);
+            var outputVariables = new VariableDictionary(outputVariablesFile);
+
+            variables.MergeWith(outputVariables);
             variables.EnrichWithEnvironmentVariables();
             variables.LogVariables();
 
-            return InvokeScript(variables);
+            return InvokeScript(variables, outputVariables);
         }
         
-        private int InvokeScript(CalamariVariableDictionary variables)
+        private int InvokeScript(CalamariVariableDictionary variables, VariableDictionary outputVariables)
         {
             if (!File.Exists(scriptFile))
                 throw new CommandException("Could not find script file: " + scriptFile);
 
             var scriptEngine = new CombinedScriptEngine();
             var runner = new CommandLineRunner(
-                new SplitCommandOutput(new ConsoleCommandOutput(), new ServiceMessageCommandOutput(variables)));
+                new SplitCommandOutput(new ConsoleCommandOutput(), new ServiceMessageCommandOutput(variables), new ServiceMessageCommandOutput(outputVariables)));
             var result = scriptEngine.Execute(scriptFile, variables, runner);
+
+            outputVariables.Save();
+
             return result.ExitCode;
         }
     }
